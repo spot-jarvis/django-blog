a016fd5170808b91fc679b5b972be0253a2d8438
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-09 09:37:28 +0530

    craeted the models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    catogory_name = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.catogory_name
+    
+STATUS_CHOICES = (
+    (0,"Draft"),
+    (1,"Published")
+)
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    featured_image = models.ImageField( upload_to='uploads/%Y/%m/%d', height_field=None, width_field=None, max_length=None)
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=800)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField( auto_now_add=True)
+    updated_at = models.DateTimeField( auto_now=True)
+
+    def __str__(self):
+        return self.title
