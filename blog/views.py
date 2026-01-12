b7c47618c86638846a039336d354c6ca27a9e5d1
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-12 22:59:25 +0530

    modified

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from .models import Category
+from .models import Category, Blog
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured = False,status = 'Published')
+    print(posts)
     return render(request,'blog/home.html',{
-        'contexts' : categories
+        'contexts' : categories,
+        'is_featured' : featured_posts,
+        'posts' : posts
     })
