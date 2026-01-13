2de5256632a1705a6339cf5c6e6689f3975d478b
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-13 20:38:01 +0530

    added the post_by categories

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home' )
+    path('', views.home, name='home' ),
+    path('category/<int:category_id>/' , views.posts_by_category , name = 'posts_by_category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,16 @@ def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False,status = 'Published')
-    print(posts)
     return render(request,'blog/home.html',{
         'contexts' : categories,
         'is_featured' : featured_posts,
         'posts' : posts
+    })
+
+def posts_by_category(request,category_id):
+    posts = Blog.objects.filter(category_id = category_id, status = 'Published').order_by('updated_at')
+    category = Category.objects.get(pk=category_id)
+    return render(request, "blog/tech.html",{
+        "posts" : posts,
+        'category' : category
     })
