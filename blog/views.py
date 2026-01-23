8d01256d4c077a4a58d59276ea4ab3c37833c320
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-23 22:13:55 +0530

    added error page

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ def home(request):
     categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False,status = 'Published')
+    if not categories:
+        return render(request,'blog/404.html')
     return render(request,'blog/home.html',{
         'contexts' : categories,
         'is_featured' : featured_posts,
@@ -15,6 +17,8 @@ def home(request):
 def posts_by_category(request,category_id):
     posts = Blog.objects.filter(category_id = category_id, status = 'Published').order_by('updated_at')
     category = get_object_or_404(Category, pk = category_id)
+    if not posts:
+        return render(request,'blog/404.html')
     return render(request, "blog/tech.html",{
         "posts" : posts,
         'category' : category
