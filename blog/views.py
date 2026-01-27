7c11859c2c21fbc7e38ea2bffba5349934523a8c
spot-jarvis <shefinsharafudheen.s@gmail.com>
2026-01-27 07:59:30 +0530

    added the context_processors

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import Category, Blog
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False,status = 'Published')
-    if not categories:
-        return render(request,'blog/404.html')
     return render(request,'blog/home.html',{
-        'contexts' : categories,
         'is_featured' : featured_posts,
         'posts' : posts
     })
@@ -17,8 +13,6 @@ def home(request):
 def posts_by_category(request,category_id):
     posts = Blog.objects.filter(category_id = category_id, status = 'Published').order_by('updated_at')
     category = get_object_or_404(Category, pk = category_id)
-    if not posts:
-        return render(request,'blog/404.html')
     return render(request, "blog/tech.html",{
         "posts" : posts,
         'category' : category
